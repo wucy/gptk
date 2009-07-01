827d6a8dc7fcfcff5b155581df5347edb817697a
remi.barillec <remi.barillec@22a8bec4-623d-11de-bed1-e3a98b22654c>
2009-07-01 16:21:10 +0000

    * Added test suite for SCGModelTrainer

---
+++ gptk/tests/TestOptimisationSCG.h
@@ -0,0 +1,41 @@
+/*
+ * TestOptimisationSCG.h
+ *
+ *  Created on: 27-Jun-2009
+ *      Author: barillrl
+ */
+
+#ifndef TESTOPTIMISATIONSCG_H_
+#define TESTOPTIMISATIONSCG_H_
+
+#include "Test.h"
+#include "optimisation/Optimisable.h"
+#include "optimisation/SCGModelTrainer.h"
+#include <cassert>
+
+class QuadraticError : public Optimisable
+{
+public:
+	QuadraticError(double x0, double y0);
+	~QuadraticError();
+
+	double 	objective() const;
+	vec 	gradient() const;
+	vec 	getParametersVector() const;
+	void 	setParametersVector(const vec p);
+
+private:
+	double x;
+	double y;
+};
+
+class TestOptimisationSCG: public Test
+{
+public:
+	TestOptimisationSCG();
+	virtual ~TestOptimisationSCG();
+
+	static bool testQuadraticError();
+};
+
+#endif /* TESTOPTIMISATIONSCG_H_ */
